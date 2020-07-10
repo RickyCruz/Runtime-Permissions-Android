bf6d5bb73604e6b7176f90632168aee0457d6bfe
Ricky Cruz <rczdev@gmail.com>
2020-07-10 18:10:16 -0500

    Multiple permissions

--- app/src/main/java/com/example/runtimepermissionswithdexter/MainActivity.kt
+++ app/src/main/java/com/example/runtimepermissionswithdexter/MainActivity.kt
@@ -7,10 +7,12 @@ import android.widget.TextView
 import androidx.core.content.ContextCompat
 import com.example.runtimepermissionswithdexter.enum.PermissionStatusEnum
 import com.karumi.dexter.Dexter
+import com.karumi.dexter.MultiplePermissionsReport
 import com.karumi.dexter.PermissionToken
 import com.karumi.dexter.listener.PermissionDeniedResponse
 import com.karumi.dexter.listener.PermissionGrantedResponse
 import com.karumi.dexter.listener.PermissionRequest
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener
 import com.karumi.dexter.listener.single.PermissionListener
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -22,6 +24,7 @@ class MainActivity : Activity() {
         btnCamera.setOnClickListener { checkCameraPermissions() }
         btnContacts.setOnClickListener { checkContactsPermissions() }
         btnAudio.setOnClickListener { checkAudioPermissions() }
+        btnAll.setOnClickListener { checkAllPermissions() }
     }
 
     private fun checkCameraPermissions() = setPermissionHandler(Manifest.permission.CAMERA, tvCamera)
@@ -29,6 +32,65 @@ class MainActivity : Activity() {
     private fun checkContactsPermissions() = setPermissionHandler(Manifest.permission.READ_CONTACTS, tvContacts)
 
     private fun checkAudioPermissions() = setPermissionHandler(Manifest.permission.RECORD_AUDIO, tvAudio)
+
+    private fun checkAllPermissions() {
+        Dexter.withContext(this)
+            .withPermissions(
+                Manifest.permission.CAMERA,
+                Manifest.permission.READ_CONTACTS,
+                Manifest.permission.RECORD_AUDIO
+            )
+            .withListener(object: MultiplePermissionsListener {
+                override fun onPermissionsChecked(report: MultiplePermissionsReport?) {
+                    report?.let {
+                        for (permission in report.grantedPermissionResponses) {
+                            when (permission.permissionName) {
+                                Manifest.permission.CAMERA -> setPermissionStatus(tvCamera, PermissionStatusEnum.GRANTED)
+                                Manifest.permission.READ_CONTACTS -> setPermissionStatus(tvContacts, PermissionStatusEnum.GRANTED)
+                                Manifest.permission.RECORD_AUDIO -> setPermissionStatus(tvAudio, PermissionStatusEnum.GRANTED)
+                            }
+                        }
+
+                        for (permission in report.deniedPermissionResponses) {
+                            when (permission.permissionName) {
+                                Manifest.permission.CAMERA -> {
+                                    if (permission.isPermanentlyDenied) {
+                                        setPermissionStatus(tvCamera, PermissionStatusEnum.PERMANENTLY_DENIED)
+                                    } else {
+                                        setPermissionStatus(tvCamera, PermissionStatusEnum.DENIED)
+                                    }
+                                }
+
+                                Manifest.permission.READ_CONTACTS -> {
+                                    if (permission.isPermanentlyDenied) {
+                                        setPermissionStatus(tvContacts, PermissionStatusEnum.PERMANENTLY_DENIED)
+                                    } else {
+                                        setPermissionStatus(tvContacts, PermissionStatusEnum.DENIED)
+                                    }
+                                }
+
+                                Manifest.permission.RECORD_AUDIO -> {
+                                    if (permission.isPermanentlyDenied) {
+                                        setPermissionStatus(tvAudio, PermissionStatusEnum.PERMANENTLY_DENIED)
+                                    } else {
+                                        setPermissionStatus(tvAudio, PermissionStatusEnum.DENIED)
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+
+                override fun onPermissionRationaleShouldBeShown(
+                    permissions: MutableList<PermissionRequest>?,
+                    token: PermissionToken?
+                ) {
+                    token?.continuePermissionRequest()
+                }
+
+            })
+            .check()
+    }
 
     private fun setPermissionHandler(permission: String, textView: TextView) {
         Dexter.withContext(this)
